9b96a14a439df22d4a623d6c9c671a0678ee6ca7
jaji12345 <sanchirwork@gmail.com>
2024-12-19 14:33:06 +0800

    first

--- back-end/index.js
+++ back-end/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "", //password
+  password: "Sanchir2007", //password
   database: "e_commerse",
 });
 
@@ -37,17 +37,93 @@ app.get("/users", (req, res) => {
   });
 });
 
+app.get("/products", (req, res) => {
+  const query = "SELECT * FROM products";
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching users:", err);
+      res.status(500).send("Error fetching users");
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get("/orders", (req, res) => {
+  const query = "SELECT * FROM orders";
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching users:", err);
+      res.status(500).send("Error fetching users");
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get("/reviews", (req, res) => {
+  const query = "SELECT * FROM reviews";
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Error fetching users:", err);
+      res.status(500).send("Error fetching users");
+    } else {
+      res.json(results);
+    }
+  });
+});
+
 // Create a new user
 app.post("/createUsers", (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, description, price, stock } = req.body;
   const query =
-    "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
-  db.query(query, [name, email, password], (err, result) => {
+    "INSERT INTO users (username, description, price, stock) VALUES (?, ?, ?)";
+  db.query(query, [name, description, price, stock], (err, result) => {
     if (err) {
       console.error("Error creating user:", err);
       res.status(500).send("Error creating user");
     } else {
       res.status(201).json({ id: result.insertId, name, email });
+    }
+  });
+});
+app.post("/createProducts", (req, res) => {
+  const { product_name, description, price, stock } = req.body;
+  const query =
+    "INSERT INTO products (product_name, description, price, stock) VALUES (?, ?, ?, ?)";
+  db.query(query, [product_name, description, price, stock], (err, result) => {
+    if (err) {
+      console.error("Error creating user:", err);
+      res.status(500).send("Error creating user");
+    } else {
+      res.status(201).json({ id: result.insertId, product_name, description, price, stock });
+    }
+  });
+});
+
+app.post("/createOrders", (req, res) => {
+  const { id, amount, status } = req.body;
+  const query =
+    "INSERT INTO users (id, amount, status) VALUES (?, ?, ?)";
+  db.query(query, [id, amount, status], (err, result) => {
+    if (err) {
+      console.error("Error creating user:", err);
+      res.status(500).send("Error creating user");
+    } else {
+      res.status(201).json({ id: result.insertId, id, amount, status });
+    }
+  });
+});
+app.post("/createOrders", (req, res) => {
+  const { order_id, amount, status } = req.body;
+  const query =
+    "INSERT INTO orders (order_id, description, price, stock) VALUES (?, ?, ?)";
+  db.query(query, [order_id, amount, status], (err, result) => {
+    if (err) {
+      console.error("Error creating user:", err);
+      res.status(500).send("Error creating user");
+    } else {
+      res.status(201).json({ id: result.insertId, id, amount, status });
     }
   });
 });
